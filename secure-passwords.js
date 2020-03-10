004efea7d60fcca7e5e72b6b4f3edb29eb0173af
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-03-09 23:28:17 -0500

    add method validations checkeds

--- secure-passwords.js
+++ secure-passwords.js
@@ -1,43 +1,67 @@
-const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowercase = "abcdefghijklmnopqrstuvwxyz";
-const numbers = "0123456789";
-const symbols = "!@#$%^&*()_+";
-
+/**
+ * @file Script para generar contraseñas seguras.
+ * @author Gonzalo Chacaltana <gchacaltanab@outlook.com>
+ * @class SecurePassword
+ * @copyright Itensoft Digital Solutions 2020
+ * @version 1.1
+*/
 const SecurePassword = {
+    password_length: 6,
+    password_counter: 1,
+    allowedValuesString: '',
+    allowedValues: {
+        uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        lowercase: 'abcdefghijklmnopqrstuvwxyz',
+        numbers: '0123456789',
+        symbols: '!@#$%^&*()_+'
+    },
+    validateChecked: function(){
+        const chkboxes = document.getElementsByClassName("custom-control-input");
+        let numberChecks = 0;
+        Array.prototype.forEach.call(chkboxes, function(el) {
+            if (el.checked) {
+                numberChecks+=1;
+            }
+        });
+        if (numberChecks==0) {
+            throw new Error("Debe seleccionar al menos una opción de valores permitidos.");
+        }
+    },
     generate: function () {
+        this.validateChecked();    
+        this.password_length = parseInt(document.getElementById("password_length").value);
+        this.password_counter = parseInt(document.getElementById("password_count").value);
         const ul = document.getElementById('ul_passwords');
         ul.innerHTML = '';
-        const count = parseInt(document.getElementById("count_passwords").value);
-        console.log(count);
-        for (let i = 0; i < count; i++) {
+        this.getAllowedValuesString();
+        for (let i = 0; i < this.password_counter; i++) {
             let li = document.createElement('li');
             li.className = "list-group-item d-flex justify-content-between lh-condensed";
-            li.textContent = SecurePassword.getPassword();
+            li.textContent = this.getPassword();
             ul.appendChild(li);
+        }
+        document.getElementById("span_pass_number").textContent = this.password_counter;
+    },
+    getAllowedValuesString: function () {
+        if (document.getElementById("chk_uppercase").checked) {
+            this.allowedValuesString += this.allowedValues.uppercase;
+        }
+        if (document.getElementById("chk_lowercase").checked) {
+            this.allowedValuesString += this.allowedValues.lowercase;
+        }
+        if (document.getElementById("chk_numbers").checked) {
+            this.allowedValuesString += this.allowedValues.numbers;
+        }
+        if (document.getElementById("chk_symbols").checked) {
+            this.allowedValuesString += this.allowedValues.symbols;
         }
     },
     getPassword: function () {
-        let allowed = '';
-        if (document.getElementById("chk_uppercase").checked) {
-            allowed += uppercase;
-        }
-        if (document.getElementById("chk_lowercase").checked) {
-            allowed += lowercase;
-        }
-        if (document.getElementById("chk_numbers").checked) {
-            allowed += numbers;
-        }
-        if (document.getElementById("chk_symbols").checked) {
-            allowed += symbols;
-        }
-
         let password = '';
-        let strength = parseInt(document.getElementById("length_password").value);
-        for (var i = 0; i < strength; i++) {
-            let random = Math.floor(Math.random() * allowed.length);
-            password += allowed[random];
+        for (let i = 0; i < this.password_length; i++) {
+            let random = Math.floor(Math.random() * this.allowedValuesString.length);
+            password += this.allowedValuesString[random];
         }
-
         return password;
     }
 };
